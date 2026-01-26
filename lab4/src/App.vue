b93c8a6fa15b1f26de61901e3a6cffbb279e9c2f
long <longdnts01621@gmail.com>
2026-01-27 00:34:51 +0700

    lab4

---
+++ lab4/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import bai4 from './components/bai4.vue';
+</script>
+
+<template>
+<bai4></bai4>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
